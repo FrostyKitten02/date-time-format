0e665e4d4c4feef2b882b957d4b7836be7f2e593
FrostyKitten02 <alen.fridau02@gmail.com>
2023-12-15 19:50:39 +0100

    removed main

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-        DateTime d = DateTime.fromByteArray(new byte[] {0, 0, 0, 0, 0, 0, 0});
-    }
-}
